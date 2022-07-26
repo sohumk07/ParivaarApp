2ad8204157e07c1e6a92babac3d2a6668abe2111
sohum <sohumsak@gmail.com>
2022-07-26 12:13:04 -0700

    7/26

--- app/src/main/java/com/example/parivaarapp/AdminPresetData.java
+++ app/src/main/java/com/example/parivaarapp/AdminPresetData.java
@@ -1,14 +1,19 @@
 package com.example.parivaarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class AdminPresetData extends AppCompatActivity {
+    private RecyclerView mFirestoreList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_preset_data);
+
+
+        mFirestoreList = findViewById(R.id.firestore_list);
     }
 }
