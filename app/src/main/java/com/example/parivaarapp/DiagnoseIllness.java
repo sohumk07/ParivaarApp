47e1420dbdf5d54a69b2b7e21c943b966652fc11
sohum <sohumsak@gmail.com>
2022-07-27 11:12:07 -0700

    7/26

--- app/src/main/java/com/example/parivaarapp/DiagnoseIllness.java
+++ app/src/main/java/com/example/parivaarapp/DiagnoseIllness.java
@@ -2,14 +2,160 @@ package com.example.parivaarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.grpc.Compressor;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.widget.Toast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DiagnoseIllness extends AppCompatActivity {
 
     private Button button;
+    public static final String TAG = "TAG";
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    private Button upload;
+
+    EditText doctorsnotefullname, doctorsnote, doctorsadvice, medicinesused, referral, followup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +163,93 @@ public class DiagnoseIllness extends AppCompatActivity {
         setContentView(R.layout.activity_diagnose_illness);
 
 
-        button=findViewById(R.id.ok);
+        upload =findViewById(R.id.dn_upload);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        doctorsnotefullname = findViewById(R.id.dn_full_name);
+        doctorsnote = findViewById(R.id.dn_doctors_note);
+        doctorsadvice = findViewById(R.id.dn_doctors_advice);
+        medicinesused = findViewById(R.id.dn_medicines_uses);
+        followup = findViewById(R.id.dn_follow_up);
+        referral = findViewById(R.id.dn_referral);
+        upload = findViewById(R.id.dn_upload);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),DailyActivation.class));
+            finish();
+        }
+
+        upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //code
-                Intent intent = new Intent(DiagnoseIllness.this,NewPatientRegistration.class);
-                startActivity(intent);
+                final String doctorsnotefullname1 = doctorsnotefullname.getText().toString().trim();
+                final String doctorsnote1 = doctorsnote.getText().toString().trim();
+                final String doctorsadvice1 = doctorsadvice.getText().toString().trim();
+                final String medicinesused1 = medicinesused.getText().toString().trim();
+                final String followup1 = followup.getText().toString().trim();
+                final String referral1 = referral.getText().toString().trim();
+
+
+                if(TextUtils.isEmpty(doctorsnotefullname1)){
+                    doctorsnotefullname.setError("Cannot Be Empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(doctorsnote1)){
+                    doctorsnote.setError("Cannot Be Empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(doctorsadvice1)){
+                    doctorsadvice.setError("Cannot Be Empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(medicinesused1)){
+                    medicinesused.setError("Cannot Be Empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(followup1)){
+                    followup.setError("Cannot Be Empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(referral1)){
+                    referral.setError("Cannot Be Empty");
+                    return;
+                }
+
+                Map<String, Object> DailyActivation = new HashMap<>();
+                // nothing in here
+
+
+                Map<String, Object> nestedData = new HashMap<>();
+                nestedData.put("Doctor's Description Of Illness", doctorsnote.getText().toString().trim());
+                nestedData.put("Doctor's Advice", doctorsadvice.getText().toString().trim());
+                nestedData.put("Medicines Used", medicinesused.getText().toString().trim());
+                nestedData.put("Follow Up Needed?", followup.getText().toString().trim());
+                nestedData.put("Need Of Referral To Higher Center?", referral.getText().toString().trim());
+
+
+                //documents and collections
+                db.collection("Patient Registration and-or Doctor's Notes").document(doctorsnotefullname.getText().toString().trim())
+                        .update(DailyActivation)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                                Toast.makeText(DiagnoseIllness.this, "Daily Activation Uploaded to Database", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                                Toast.makeText(DiagnoseIllness.this, "Error Uploading Daily Activation to Database", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
             }
         });
     }
